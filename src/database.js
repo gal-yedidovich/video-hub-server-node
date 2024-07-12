65a7550f2021c53e03a1151ed5205c6678741ceb
Gal Yedidovich <gal.yedidovich@unity3d.com>
2024-07-12 21:43:27 +0300

    REPLACED to small docker image FIXED office episodes list

--- src/database.js
+++ src/database.js
@@ -59,7 +59,7 @@ export function saveEpisode({ show, season, episode, time }) {
 
 
 const SHOW_EPISODE_COUNT = {
-	Office: [6, 18, 23, 14, 26, 24, 24, 24, 23],
+	Office: [6, 22, 22, 14, 26, 24, 24, 24, 23],
 	Friends: [24, 24, 25, 24, 24, 25, 24, 24, 24, 18],
 	FamilyGuy: [7, 21, 22, 30, 18, 12, 16, 21, 18, 23, 22, 21, 18, 20, 20, 20, 20, 20, 20, 20],
 	'Avatar - The last airbender': [20, 20, 21],
